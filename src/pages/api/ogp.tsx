cdc9f27bab182501f5ab1ad7ae7c096f58544289
anji0114 <anji.tnk.140114@gmail.com>
2024-02-23 16:24:07 +0900

    ogpをスクショに

--- src/pages/api/ogp.tsx
+++ src/pages/api/ogp.tsx
@@ -1,54 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import ReactDOM from "react-dom/server";
 import * as playwright from "playwright-aws-lambda";
-
-const Content = (props: { title: string }) => (
-  <html>
-    <head></head>
-    <body
-      style={{
-        boxSizing: "border-box",
-      }}
-    >
-      <div>
-        <h1
-          style={{
-            textAlign: "center",
-            fontWeight: "bold",
-            padding: "40px",
-            margin: "-10px -10px 0",
-            fontSize: "18px",
-            backgroundColor: "#fafafa",
-            backgroundImage: "url(http://localhost:3000/image/image.webp)",
-            backgroundPosition: "center center",
-            backgroundSize: "cover",
-          }}
-        >
-          タイトルが入ります
-        </h1>
-        <div style={{ padding: "20px 40px", lineHeight: "1.75" }}>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi recusandae veniam
-            dignissimos explicabo velit nemo, aspernatur, magni reprehenderit perspiciatis error
-            ratione iure perferendis exercitationem voluptas dolorum debitis expedita fugiat Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Animi recusandae veniam
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi recusandae veniam
-            dignissimos explicabo velit nemo, aspernatur, magni reprehenderit perspiciatis error
-            ratione iure perferendis exercitationem voluptas dolorum debitis expedita fugiat
-            adipisci!
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi recusandae veniam
-            dignissimos explicabo velit nemo, aspernatur, magni reprehenderit perspiciati ratione
-            iure perferendis exercitationem voluptas dolorum debitis expedita fugiat adipisci!
-          </p>
-        </div>
-      </div>
-    </body>
-  </html>
-);
 
 const OGP = async (req: NextApiRequest, res: NextApiResponse) => {
   // サイズの設定
@@ -59,7 +10,7 @@ const OGP = async (req: NextApiRequest, res: NextApiResponse) => {
   const page = await browser.newPage({ viewport });
 
   // HTMLをセットして、ページの読み込み完了を待つ
-  await page.goto("https://study-repo-eta.vercel.app/ogp", { waitUntil: "networkidle" });
+  await page.goto("https://gines.jp/users/anji", { waitUntil: "networkidle" });
 
   // スクリーンショットを取得する
   const image = await page.screenshot({ type: "png" });
