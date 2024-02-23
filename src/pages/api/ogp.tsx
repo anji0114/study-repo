858eb70bf5fac4394b54c28d3858d6e06456d77a
anji0114 <anji.tnk.140114@gmail.com>
2024-02-23 15:33:39 +0900

    ogp変更

--- src/pages/api/ogp.tsx
+++ src/pages/api/ogp.tsx
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { chromium } from "playwright";
+import ReactDOM from "react-dom/server";
+import * as playwright from "playwright-aws-lambda";
 
 const styles = `
   html, body {
@@ -10,34 +11,45 @@ const styles = `
   h1 { margin: auto; color: red; }
 `;
 
-const html = `
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-  html, body {
-    height: 100%;
-    display: grid;
-  }
-  h1 { margin: auto; color: red; }
-</style>
-</head>
-<body>
-  <h1>Title Placeholder</h1>
-</body>
-</html>
-`;
+const Content = (props: { title: string }) => (
+  <html>
+    <head>
+      <style>{styles}</style>
+    </head>
+    <body>
+      <h1>{props.title}</h1>
+    </body>
+  </html>
+);
 
 const OGP = async (req: NextApiRequest, res: NextApiResponse) => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.setContent(html.replace("Title Placeholder", "Default Title")); // タイトルを動的に置換
-  const screenshot = await page.screenshot();
+  // サイズの設定
+  const viewport = { width: 1200, height: 630 };
+
+  // ブラウザインスタンスの生成
+  const browser = await playwright.launchChromium();
+  const page = await browser.newPage({ viewport });
+
+  // HTMLの生成
+  const props = { title: "Hello OGP!" };
+  const markup = ReactDOM.renderToStaticMarkup(<Content {...props} />);
+  const html = `<!doctype html>${markup}`;
+
+  // HTMLをセットして、ページの読み込み完了を待つ
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+
+  // スクリーンショットを取得する
+  const image = await page.screenshot({ type: "png" });
   await browser.close();
 
-  // スクリーンショットをレスポンスとして返す
+  // Vercel Edge Networkのキャッシュを利用するための設定
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
+
+  // Content Type を設定
   res.setHeader("Content-Type", "image/png");
-  res.send(screenshot);
+
+  // レスポンスを返す
+  res.end(image);
 };
 
 export default OGP;
